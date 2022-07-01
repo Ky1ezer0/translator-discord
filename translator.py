5a85db94c82f6bde46b6b6e7224399b39a9066f9
Kylez <kylezli@protonmail.com>
2022-07-01 22:36:18 +0800

    fix user and channel apear. edit() only watch target channel

--- translator.py
+++ translator.py
@@ -35,64 +35,100 @@ class MyClient(discord.Client):
                     message.author.id == self.admin_id
                     or message.author.guild_permissions.administrator
                 ):
+                    channel = await self.fetch_channel(self.translate_channel_id)
                     await message.channel.send(
-                        f"Currently target channel is: {self.translate_channel_id}"
+                        f"Currently target channel is: {channel.name} in {channel.guild.name}"
                     )
             case _:
                 if (
                     message.channel.id == self.translate_channel_id
                     and message.author.bot == False
                 ):
-                    try:
-                        translated_text = GoogleTranslator(
-                            source="auto", target="en"
-                        ).translate(text=message.content)
-                        try:
-                            # replace correct emoji in translated text
-                            custom_emojis_in_raw = re.findall(
-                                "<:\w*:\d*>", message.content
+                    translated_text = await self.translate(message)
+                    if translated_text != message.content and translated_text != None:
+                        if message.author.nick == None:
+                            await message.channel.send(
+                                f"**{message.author.name}** : {translated_text}"
                             )
-                            custom_emojis_in_translated = re.findall(
-                                "<: \w*: \d*>", translated_text
+                        else:
+                            await message.channel.send(
+                                f"**{message.author.nick}** : {translated_text}"
                             )
 
-                            for index, emoji in enumerate(custom_emojis_in_translated):
-                                translated_text = translated_text.replace(
-                                    emoji, custom_emojis_in_raw[index]
-                                )
-                        except TypeError:
-                            pass
+    async def on_message_edit(self, before_msg, after_msg):
+        if before_msg.channel.id == self.translate_channel_id:
+            translated_text = await self.translate(before_msg)
+            if translated_text != after_msg.content:
+                translated_text = await self.translate(after_msg)
+                if after_msg.author.nick == None:
+                    await after_msg.channel.send(
+                        f"**{after_msg.author.name}** : {translated_text}"
+                    )
+                else:
+                    await after_msg.channel.send(
+                        f"**{after_msg.author.nick}** : {translated_text}"
+                    )
 
-                        try:
-                            # animation emoji also
-                            custom_emojis_in_raw = re.findall(
-                                "<a:\w*:\d*>", message.content
-                            )
-                            custom_emojis_in_translated = re.findall(
-                                "<a: \w*: \d*>", translated_text
-                            )
+    async def translate(self, message):
+        try:
+            translated_text = GoogleTranslator(source="auto", target="en").translate(
+                text=message.content
+            )
+            try:
+                # replace correct emoji in translated text
+                custom_emojis_in_raw = re.findall("<:\w*:\d*>", message.content)
+                custom_emojis_in_translated = re.findall(
+                    "<: \w*: \d*>", translated_text
+                )
 
-                            for index, emoji in enumerate(custom_emojis_in_translated):
-                                translated_text = translated_text.replace(
-                                    emoji, custom_emojis_in_raw[index]
-                                )
-                        except TypeError:
-                            pass
+                for index, emoji in enumerate(custom_emojis_in_translated):
+                    translated_text = translated_text.replace(
+                        emoji, custom_emojis_in_raw[index]
+                    )
+            except TypeError:
+                pass
 
-                        if (
-                            translated_text != message.content
-                            and translated_text != None
-                        ):
-                            if message.author.nick == None:
-                                await message.channel.send(
-                                    f"**{message.author.name}** : {translated_text}"
-                                )
-                            else:
-                                await message.channel.send(
-                                    f"**{message.author.nick}** : {translated_text}"
-                                )
-                    except exceptions.NotValidPayload:
-                        pass
+            try:
+                # animation emoji also
+                custom_emojis_in_raw = re.findall("<a:\w*:\d*>", message.content)
+                custom_emojis_in_translated = re.findall(
+                    "<a: \w*: \d*>", translated_text
+                )
+
+                for index, emoji in enumerate(custom_emojis_in_translated):
+                    translated_text = translated_text.replace(
+                        emoji, custom_emojis_in_raw[index]
+                    )
+            except TypeError:
+                pass
+
+            try:
+                # user also
+                custom_emojis_in_raw = re.findall("<@\d*>", message.content)
+                custom_emojis_in_translated = re.findall("<@ \d*>", translated_text)
+
+                for index, emoji in enumerate(custom_emojis_in_translated):
+                    translated_text = translated_text.replace(
+                        emoji, custom_emojis_in_raw[index]
+                    )
+            except TypeError:
+                pass
+
+            try:
+                # channel also
+                custom_emojis_in_raw = re.findall("<#\d*>", message.content)
+                custom_emojis_in_translated = re.findall("<# \d*>", translated_text)
+
+                for index, emoji in enumerate(custom_emojis_in_translated):
+                    translated_text = translated_text.replace(
+                        emoji, custom_emojis_in_raw[index]
+                    )
+            except TypeError:
+                pass
+
+            return translated_text
+        except exceptions.NotValidPayload:
+            return
 
 
 intents = discord.Intents.default()
